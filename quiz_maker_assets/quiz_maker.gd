9b66c93e91176a5548a8c06c0d21111de8bf7ef0
szuhi27 <43464694+szuhi27@users.noreply.github.com>
2025-02-03 21:42:33 +0100

    image importing done
    image importing fully implemented, both drag and drop and with button import

--- quiz_maker_assets/quiz_maker.gd
+++ quiz_maker_assets/quiz_maker.gd
@@ -1,17 +1,19 @@
 extends Node
 
-
+func _ready() -> void:
+	get_tree().get_root().files_dropped.connect(_on_files_dropped)
 
 func _on_button_pressed() -> void:
 	$ImageImportGroup/FileDialog.popup()
 
-
-
-
 func _on_file_dialog_file_selected(path: String) -> void:
 	_load_image(path)
 
-
+func _on_files_dropped(files):
+	if files.size() > 1:
+		$ImageImportGroup/ErrorMsg.text = "Egyszerre egy képet válasszon!"
+	else:
+		_load_image(files[0])
 
 func _load_image(path: String):
 	var image = Image.new()
@@ -21,7 +23,7 @@ func _load_image(path: String):
 		image.load(path)
 		var image_texture = ImageTexture.new()
 		image_texture.set_image(image)
-		$QuizImage.texture = image_texture
+		$Control/QuizImage.texture = image_texture
 		$ImageImportGroup.visible = false
 	else :
 		$ImageImportGroup/ErrorMsg.text = "Nem megfelelő fájltípus!"
