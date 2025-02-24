73ba97100f296bf5a7a7c6669fbbb7c35453e431
szuhi27 <43464694+szuhi27@users.noreply.github.com>
2025-02-24 18:15:03 +0100

    placable, movable answ bubble
    working on teh answer bubble placing, still not 100%

---
+++ quiz_maker_assets/answer_bubble_contol.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var selected = false
+var mouse_offset = Vector2(0, 0)
+
+func _process(delta: float) -> void:
+	if selected:
+		position = get_global_mouse_position() + mouse_offset
+
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			mouse_offset = position - get_global_mouse_position()
+			selected = true
+		else:
+			selected = false
+
+
+func _on_delete_button_pressed() -> void:
+	queue_free()
