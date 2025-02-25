4604ea9b9ba71b61ace92ff3808a8d662cee2df9
szuhi27 <43464694+szuhi27@users.noreply.github.com>
2025-02-25 19:22:48 +0100

    made bubble movable
    made the new answer bubble movable
    there seems to be a problem with current version on Godot, so had to find a different way to move it

--- quiz_maker_assets/answer_bubble_contol.gd
+++ quiz_maker_assets/answer_bubble_contol.gd
@@ -1,22 +1,28 @@
 extends Node2D
 
 var selected = false
-var mouse_offset = Vector2(0, 0)
+var mouse_offset = Vector2(115, 23)
 
 func _process(delta: float) -> void:
 	if selected:
 		position = get_global_mouse_position() + mouse_offset
 
-
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			mouse_offset = position - get_global_mouse_position()
-			selected = true
-		else:
-			selected = false
-
+#this should work, but apparently there is a problem with the current version of gogot (4.3)
+#
+#func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		#print_debug("wn")
+		#if event.pressed:
+			#mouse_offset = position - get_global_mouse_position()
+			#selected = true
+		#else:
+			#selected = false
 
 func _on_delete_button_pressed() -> void:
 	queue_free()
+
+func _on_move_button_button_down() -> void:
+	selected = true
+
+func _on_move_button_button_up() -> void:
+	selected = false
