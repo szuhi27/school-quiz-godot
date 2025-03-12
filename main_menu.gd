c391b6f3d57b343de6b9bfb90a3b517327d11176
szuhi27 <43464694+szuhi27@users.noreply.github.com>
2025-03-12 20:41:15 +0100

    zip out json in
    saving as zip was a bad idead, swapped to saving everything in a json
    so far seems to work
    will need to clean up

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func _ready() -> void:
+	get_tree().get_root().files_dropped.connect(_on_files_dropped)
+
+func  _on_files_dropped(files):
+	var file = FileAccess.open(files[0], FileAccess.READ)
+	var json_string = file.get_as_text()
+	file.close()
+	var json = JSON.new()
+	var parse_result = json.parse(json_string)
+	var data = json.data
+	var image = Image.new()
+	image.load_jpg_from_buffer(data["image"].to_utf8_buffer())
+	var image_texture = ImageTexture.new()
+	image_texture.set_image(image)
+	$Control/TextureRect.texture = image_texture
--- quiz_maker_assets/quiz_maker.gd
+++ quiz_maker_assets/quiz_maker.gd
@@ -2,7 +2,8 @@ extends Node
 
 var _image = Image
 var _zipfile = ZIPPacker
-var _image_name
+var _image_name 
+var _image_data
 
 func _ready() -> void:
 	get_tree().get_root().files_dropped.connect(_on_files_dropped)
@@ -25,6 +26,9 @@ func _load_image(path: String):
 	
 	if try_load_image == OK:
 		_image.load(path)
+		
+		_image_data = _image.save_jpg_to_buffer() #get_buffer(_image.get_len())
+		
 		var image_texture = ImageTexture.new()
 		image_texture.set_image(_image)
 		$Control/QuizImage.texture = image_texture
@@ -45,12 +49,33 @@ func _on_save_file_dialog_file_selected(path: String) -> void:
 	_save_quiz(path)
 
 func _save_quiz(path: String):
-	_zipfile = ZIPPacker.new()
-	var err = _zipfile.open(path, _zipfile.APPEND_CREATE)
-	if err != OK:
-		return err
-	_zipfile.start_file(_image_name)
-	_zipfile.write_file(_image.get_data())
-	_zipfile.close_file()
-	_zipfile.close()
+	#_zipfile = ZIPPacker.new()
+	#var err = _zipfile.open(path, _zipfile.APPEND_CREATE)
+	#if err != OK:
+		#return err
+	#_zipfile.start_file(_image_name)
+	#_zipfile.write_file(_image.get_data())
+	#_zipfile.close_file()
+	#_zipfile.close()
 	
+	var Im = Image.new()
+	var base64_string = Marshalls.raw_to_base64(_image_data)
+	var byt = Marshalls.base64_to_raw(base64_string)
+	Im.load_jpg_from_buffer(byt)
+	var image_texture = ImageTexture.new()
+	image_texture.set_image(Im)
+	$TestRect.texture = image_texture
+	
+	var json_data = {}
+	#var imdat = _image.get_data()
+	json_data["image"] = base64_string  # imdat
+	
+	var json = JSON.new()
+	var json_string = json.stringify(json_data)
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_string(json_string)
+	file.close()
+	
+	
+
+#a képet is json ba mneteni, ezt az iinfo reszt, és akkor csak egy dilet kell létrehozni, próbáljuk
